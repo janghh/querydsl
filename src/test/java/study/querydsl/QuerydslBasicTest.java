5063c3709cbfa0b091deafcf757cefb016b23710
pf3-janghh <janghh@ex-em.com>
2023-06-17 16:35:09 +0900

    섹션 3. 기본 문법 - 결과 조회

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,167 @@
+package study.querydsl;
+
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+    @PersistenceContext
+    EntityManager em;
+
+    /*
+    JPAQueryFactory를 필드로 제공하면 동시성 문제는 어떻게 될까?
+    동시성 문제는 JPAQueryFactory를 생성할 때 제공하는 EntityManager(em)에 달려있다.
+    스프링 프레임워크는 여러 쓰레드에서 동시에 같은 EntityManager에 접근해도,
+    트랜잭션 마다 별도의 영속성 컨텍스트를 제공하기 때문에,
+    동시성 문제는 걱정하지 않아도 된다
+     */
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        //member1을 찾아라.
+        String qlString =
+                "select m from Member m " +
+                        "where m.username = :username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+
+        // QMember qMember = new QMember("m"); //별칭 직접 지정
+        // QMember qMember = QMember.member; //기본 인스턴스 사용
+
+        //member1을 찾아라.
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")) //파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    /*
+    JPQL이 제공하는 모든 검색 조건 제공
+    member.username.eq("member1") // username = 'member1'
+    member.username.ne("member1") //username != 'member1'
+    member.username.eq("member1").not() // username != 'member1'
+    member.username.isNotNull() //이름이 is not null
+    member.age.in(10, 20) // age in (10,20)
+    member.age.notIn(10, 20) // age not in (10, 20)
+    member.age.between(10,30) //between 10, 30
+    member.age.goe(30) // age >= 30
+    member.age.gt(30) // age > 30
+    member.age.loe(30) // age <= 30
+    member.age.lt(30) // age < 30
+    member.username.like("member%") //like 검색
+    member.username.contains("member") // like ‘%member%’ 검색
+    member.username.startsWith("member") //like ‘member%’ 검색
+     */
+
+    @Test
+    public void searchAndParam() {
+        List<Member> result1 = queryFactory
+                .selectFrom(member)
+                // where() 에 파라미터로 검색조건을 추가하면 AND 조건이 추가됨
+                // 이 경우 null 값은 무시 메서드 추출을 활용해서 동적 쿼리를 깔끔하게 만들 수 있음 뒤에서 설명
+                .where(member.username.eq("member1"),
+                        member.age.eq(10))
+                .fetch();
+
+        assertThat(result1.size()).isEqualTo(1);
+    }
+
+    /*
+    결과 조회
+        fetch() : 리스트 조회, 데이터 없으면 빈 리스트 반환
+        fetchOne() : 단 건 조회
+        결과가 없으면 : null
+        결과가 둘 이상이면 : com.querydsl.core.NonUniqueResultException
+        fetchFirst() : limit(1).fetchOne()
+        fetchResults() : 페이징 정보 포함, total count 쿼리 추가 실행
+        fetchCount() : count 쿼리로 변경해서 count 수 조회
+     */
+    @Test
+    public void fetch_fetchOne_fetchFirst_fetchResults_fetchCount(){
+        //List
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        //단 건
+        Member findMember1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        //처음 한 건 조회
+        Member findMember2 = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        //페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        //count 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+}
