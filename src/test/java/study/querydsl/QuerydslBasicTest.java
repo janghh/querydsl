b52b38f4ce22823593f810b8489282213723bf12
pf3-janghh <janghh@ex-em.com>
2023-06-17 17:23:57 +0900

    섹션 3. 기본 문법 - 조인(기본조인, on절, 페치조인), 서브쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,16 +2,20 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
@@ -275,7 +279,257 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
 
-
     }
 
-}
+    /**
+     * 조인 - 기본 조인
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() throws Exception {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+
+        /*
+        join() , innerJoin() : 내부 조인(inner join)
+        leftJoin() : left 외부 조인(left outer join)
+        rightJoin() : rigth 외부 조인(rigth outer join)
+        JPQL의 on 과 성능 최적화를 위한 fetch 조인 제공 다음 on 절에서 설명
+        */
+    }
+
+    /**
+     * 세타 조인(연관관계가 없는 필드로 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+
+        //from 절에 여러 엔티티를 선택해서 세타 조인
+        //외부 조인 불가능 다음에 설명할 조인 on을 사용하면 외부 조인 가능
+    }
+
+    /*
+    조인 - on절
+    ON절을 활용한 조인(JPA 2.1부터 지원)
+        1. 조인 대상 필터링
+        2. 연관관계 없는 엔티티 외부 조인
+     */
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN m.team t on t.name = 'teamA'
+     * SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.TEAM_ID=t.id and
+     t.name='teamA'
+     */
+    @Test
+    public void join_on_filtering() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                    .leftJoin(member.team, team) // 외부조인(leftJoin) + on: 팀이 존재하지 않으면 필터링X (존재하는 경우에만 on 조건 적용)
+//                    .join(member.team, team) // 내부조인(join) + on: 팀이 존재하지 않으면 필터링O
+                        .on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+        // 참고: on 절을 활용해 조인 대상을 필터링 할 때,
+        // 외부조인이 아니라 내부조인(inner join)을 사용하면, where 절에서 필터링 하는 것과 기능이 동일하다.
+        // 따라서 on 절을 활용한 조인 대상 필터링을 사용할 때, 내부조인 이면 익숙한 where 절로 해결하고,
+        // 정말 외부조인이 필요한 경우에만 이 기능(외부조인)을 사용하자
+    }
+
+    // 2. 연관관계 없는 엔티티 외부 조인
+    // 예) 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+    /**
+     * 2. 연관관계 없는 엔티티 외부 조인
+     * 예) 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN Team t on m.username = t.name
+     * SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.username = t.name
+     */
+    @Test
+    public void join_on_no_relation() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                    .leftJoin(team) // on조인
+                        .on(member.username.eq(team.name))
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("t=" + tuple);
+        }
+
+        // 하이버네이트 5.1부터 on 을 사용해서 서로 관계가 없는 필드로 외부 조인하는 기능이 추가되었다. 물론 내부 조인도 가능하다.
+        // 주의! 문법을 잘 봐야 한다. leftJoin() 부분에 일반 조인과 다르게 엔티티 하나만 들어간다.
+        // 일반조인: leftJoin(member.team, team)
+        // on조인: from(member).leftJoin(team).on(xxx)
+    }
+
+    /*
+    조인 - 페치 조인
+     - 페치 조인은 SQL에서 제공하는 기능은 아니다. SQL조인을 활용해서 연관된 엔티티를 SQL 한번에 조회하는 기능이다.
+        주로 성능 최적화에 사용하는 방법이다.
+     */
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    //  페치 조인 미적용
+    //  지연로딩으로 Member, Team SQL 쿼리 각각 실행
+    @Test
+    public void fetchJoinNo() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+    }
+
+    // 페치 조인 적용
+    // 즉시로딩으로 Member, Team SQL 쿼리 조인으로 한번에 조회
+    @Test
+    public void fetchJoinUse() throws Exception {
+        em.flush();
+        em.clear();
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        boolean loaded =
+                emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+
+        // 사용방법: join(), leftJoin() 등 조인 기능 뒤에 fetchJoin() 이라고 추가하면 된다.
+        // 참고: 페치 조인에 대한 자세한 내용은 JPA 기본편이나, 활용2편을 참고하자
+    }
+
+    // 서브 쿼리
+    // com.querydsl.jpa.JPAExpressions 사용
+    /**
+     * 서브 쿼리 eq 사용
+     * 나이가 가장 많은 회원 조회
+     */
+    @Test
+    public void subQuery() throws Exception {
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                        JPAExpressions // JPAExpressions
+                                .select(memberSub.age.max())
+                                .from(memberSub)
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(40);
+    }
+
+    /**
+     * 서브 쿼리 goe 사용
+     * 나이가 평균 나이 이상인 회원
+     */
+    @Test
+    public void subQueryGoe() throws Exception {
+        QMember memberSub = new QMember("memberSub");
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.goe(
+                        JPAExpressions
+                                .select(memberSub.age.avg())
+                                .from(memberSub)
+                ))
+                .fetch();
+        assertThat(result).extracting("age")
+                .containsExactly(30,40);
+    }
+
+    /**
+     * 서브쿼리 여러 건 처리, in 사용
+     */
+    @Test
+    public void subQueryIn() throws Exception {
+        QMember memberSub = new QMember("memberSub");
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.in(
+                        JPAExpressions
+                                .select(memberSub.age)
+                                .from(memberSub)
+                                .where(memberSub.age.gt(10))
+                ))
+                .fetch();
+        assertThat(result).extracting("age")
+                .containsExactly(20, 30, 40);
+    }
+
+    /*
+    select 절에 subquery
+    List<Tuple> fetch = queryFactory
+     .select(member.username,
+     JPAExpressions
+     .select(memberSub.age.avg())
+     .from(memberSub)
+     ).from(member)
+     .fetch();
+    for (Tuple tuple : fetch) {
+     System.out.println("username = " + tuple.get(member.username));
+     System.out.println("age = " +
+    tuple.get(JPAExpressions.select(memberSub.age.avg())
+     .from(memberSub)));
+    }
+     */
+
+    // static import 활용
+//    import static com.querydsl.jpa.JPAExpressions.select;
+//    List<Member> result = queryFactory
+//            .selectFrom(member)
+//            .where(member.age.eq(
+//                    select(memberSub.age.max())
+//                            .from(memberSub)
+//            ))
+//            .fetch()
+
+    /*
+    <from 절의 서브쿼리 한계>
+        JPA JPQL 서브쿼리의 한계점으로 from 절의 서브쿼리(인라인 뷰)는 지원하지 않는다.
+        당연히 Querydsl도 지원하지 않는다.
+        하이버네이트 구현체를 사용하면 select 절의 서브쿼리는 지원한다.
+        Querydsl도 하이버네이트 구현체를 사용하면 select 절의 서브쿼리를 지원한다.
+     */
+
+    /*
+    <from 절의 서브쿼리 해결방안>
+        1. 서브쿼리를 join으로 변경한다. (가능한 상황도 있고, 불가능한 상황도 있다.)
+        2. 애플리케이션에서 쿼리를 2번 분리해서 실행한다.
+        3. nativeSQL을 사용한다.
+     */
+
+} //end class
